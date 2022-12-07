5bcbbd83efc5868dc13b683b9d577cbb51a2145d
banana2093 <spacebananan@gmail.com>
2022-12-07 11:50:50 +0900

    ss

--- 06.React학습/react-app/my-react-app/src/DC/Layout.js
+++ 06.React학습/react-app/my-react-app/src/DC/Layout.js
@@ -1,9 +1,10 @@
-import { Outlet, Link } from "react-router-dom";
+import {Outlet, Link} from "react-router-dom";
 import Logo from "./Logo";
 import "./Layout.css";
-/*
+// import "https://kit.fontawesome.com/a076d05399.js";
+/* 
     [ 리엑트 스타일링 기법 3가지 ]
-    1. 일반 css파일을 컴포넌트 JS파일에 import하여 사용
+    1. 일반 CSS파일을 컴포넌트 JS파일에 import하여 사용
     예) import "CSS경로";
 
     2. 객체를 만들어서 적용하는 방법
@@ -12,57 +13,63 @@ import "./Layout.css";
     <h1 style={mystyle}>하하하</h1>
 
     3. 직접 중괄호 표현식에 중괄호를 하나 더 하여 표현
-    -> 중괄호 하나는 표현식, 안에 중괄호는 객체(css용)
+    -> 중괄호 하나는 표현식, 안에 중괄호는 객체(CSS용)
     예)
-     <h1 style={{color:"red"}}>하하하</h1>
+    <h1 style={{color:"red"}}>하하하</h1>
 */
 
 const Layout = () => {
-    return (
+    return(
         <>
-            {/* 1. 상단영역 */}
-            <header className="top">
-                {/* 네비게이션 파트 */}
-                <nav className="gnb">
-                    <ul>
-                        <li>
-                            <Logo />
-                        </li>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/ct">CHARACTERS</Link>
-                        </li>
-                        <li>
-                            <Link to="/co">COMICS</Link>
-                        </li>
-                        <li>
-                            <Link to="/mv">MOVIES</Link>
-                        </li>
-                        <li>
-                            <Link to="/gm">GAMES</Link>
-                        </li>
-                        <li>
-                            <Link to="/vd">VIDEO</Link>
-                        </li>
-                        <li>
-                            <Link to="/nw">NEWS</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            {/* 2. 메인영역 */}
-            <main className="cont">
-                {/* 출력파트 : 각 페이지가 표시됨 */}
-                <Outlet />
-            </main>
-            {/* 3. 하단영역 */}
-            <footer className="info">
-                All Site Content © &amp; TM DC, unless otherwise noted here. 
-                <br/>
-                All rights reserved.
-            </footer>
+        {/* 1.상단영역 */}
+        <header className="top">
+        {/* 네비게이션 파트 */}
+            <nav className="gnb">
+                <ul>
+                    <li>
+                        <Logo />
+                    </li>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/ct">CHARACTERS</Link>
+                    </li>
+                    <li>
+                        <Link to="/co">COMICS</Link>
+                    </li>
+                    <li>
+                        <Link to="/mv">MOVIES</Link>
+                    </li>
+                    <li>
+                        <Link to="/gm">GAMES</Link>
+                    </li>
+                    <li>
+                        <Link to="/vd">VIDEO</Link>
+                    </li>
+                    <li>
+                        <Link to="/nw">NEWS</Link>
+                    </li>
+                    <li>
+                        <Link to="/mem">
+                            <i className='fas fa-users' style={{fontSize:"36px",color:"lime"}}></i>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        {/* 2.메인영역 */}
+        <main className="cont">         
+            {/* 출력파트 : 각 페이지가 표시됨 */}
+            <Outlet />
+        </main>
+        {/* 3.하단영역 */}
+        <footer className="info">
+            All Site Content © &amp; TM DC, unless otherwise noted here.
+            <br /> 
+            All rights reserved. 
+        </footer>
+           
         </>
     );
 };
